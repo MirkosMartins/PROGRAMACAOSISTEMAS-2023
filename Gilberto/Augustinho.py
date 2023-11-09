510525aa87a2376d278c74ded76c8d093369de31
Gilberto <124392451+eumorales@users.noreply.github.com>
2023-11-09 11:07:48 -0300

    Add files via upload

---
+++ Gilberto/Augustinho.py
@@ -0,0 +1,122 @@
+registros = [0] * 10
+
+
+def chamar_memoria():
+
+  global registros
+  print(registros)
+
+
+def memoria():
+
+  global registros
+  registros = [0] * 10
+
+
+def executar_comando(comando):
+
+  global registros
+
+  elementos = comando.split(' ')
+
+  if len(elementos) == 2:
+
+    param = elementos[1].split(',')
+    p1 = param[0]
+    p2 = param[1]
+
+    if elementos[0] == 'LOAD':
+
+      if p2.startswith('R'):
+
+        posicao_origem = int(p2[1:])
+        posicao_destino = int(p1[1:])
+        registros[posicao_destino] = registros[posicao_origem]
+
+      else:
+
+        posicao = int(p1[1:])
+        registros[posicao] = int(p2)
+
+    elif elementos[0] == 'ADD':
+
+      if p2.startswith('R'):
+
+        posicaoOrigem = int(p2[1:])
+        posicaoDestino = int(p1[1:])
+        registros[posicaoDestino] += registros[posicaoOrigem]
+
+      else:
+
+        posicao = int(p1[1:])
+        registros[posicao] += int(p2)
+
+    elif elementos[0] == 'SUB':
+
+      if p2.startswith('R'):
+
+        posicaoOrigem = int(p2[1:])
+        posicaoDestino = int(p1[1:])
+        registros[posicaoDestino] -= registros[posicaoOrigem]
+
+      else:
+
+        posicao = int(p1[1:])
+        registros[posicao] -= int(p2)
+
+    elif elementos[0] == 'MULT':
+
+      if p2.startswith('R'):
+
+        posicaoOrigem = int(p2[1:])
+        posicaoDestino = int(p1[1:])
+        registros[posicaoDestino] *= registros[posicaoOrigem]
+
+      else:
+
+        posicao = int(p1[1:])
+        registros[posicao] *= int(p2)
+
+    elif elementos[0] == 'DIV':
+
+      if p2.startswith('R'):
+
+        posicao_origem = int(p2[1:])
+        posicao_destino = int(p1[1:])
+
+        if registros[posicao_origem] != 0:
+
+          registros[posicao_destino] /= registros[posicao_origem]
+
+        else:
+
+          print("A divisão não pode ser por zero.")
+
+      else:
+
+        posicao = int(p1[1:])
+
+        if int(p2) != 0:
+
+          registros[posicao] /= int(p2)
+
+        else:
+
+          print("A divisão não pode ser por zero.")
+          break
+
+    chamar_memoria()
+
+  else:
+
+    print("Comando inválido.")
+
+
+while True:
+
+  cmd = input('\n\nComandos disponíveis:\nLOAD\nADD\nSUB\nDIV\nMULT\n\nInforme o comando para executar: ')
+
+  if cmd == 'HALT':
+    break
+
+  executar_comando(cmd)
