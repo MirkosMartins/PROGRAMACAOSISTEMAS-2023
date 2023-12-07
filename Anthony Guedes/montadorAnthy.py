8033148ba5276deae65c19b4869e1d4cdbb655ac
AnthyGuedes <142349999+AnthyGuedes@users.noreply.github.com>
2023-12-07 10:40:34 -0300

    Update montadorAnthy.py

--- Anthony Guedes/montadorAnthy.py
+++ Anthony Guedes/montadorAnthy.py
@@ -4,126 +4,173 @@ labels = {}
 
 
 def mostraMemoria():
-    global registros
-    print(registros)
+  global registros
+  print(registros)
 
 
 def memoria():
-    global registros
-    for i in range(10):
-        registros.append(0)
-    return registros
-
+  global registros
+  for i in range(10):
+    registros.append(0)
+  return registros
 
 def executaComando(cmd):
-    global registros
-    elementos = cmd.split(' ')
-    if len(elementos) == 3:
-        if ',' in elementos[2]:
-            param = elementos[2].split(',')
-            p1 = param[0]
-            p2 = param[1]
-            posicaoDestino = int(p1[1:])
-            if p2.startswith('R'):
-                valorOrigem = registros[int(p2[1:])]
-            else:
-                valorOrigem = int(p2)
-            if elementos[1] == 'LOAD':
-                registros[posicaoDestino] = valorOrigem
-            elif elementos[1] == 'ADD':
-                registros[posicaoDestino] = registros[posicaoDestino] + valorOrigem
-            elif elementos[1] == 'SUB':
-                registros[posicaoDestino] = registros[posicaoDestino] - valorOrigem
-            elif elementos[1] == 'MULT':
-                registros[posicaoDestino] = registros[posicaoDestino] * valorOrigem
-            elif elementos[1] == 'DIV':
-                if valorOrigem != 0:
-                    registros[posicaoDestino] = int(registros[posicaoDestino] / valorOrigem)
-                else:
-                    print('Divisao por zero. ERRO')
-                    return -1
-            elif elementos[1] == 'CMP':
-                # compara os valores e atualiza um registro  ex(, R9)
-                if registros[posicaoDestino] == valorOrigem:
-                    registros[9] = 0  # valores =, status = 0
-                elif registros[posicaoDestino] < valorOrigem:
-                    registros[9] = -1  # valor destino é menor, status = -1
-                else:
-                    registros[9] = 1  # valor destino é maior, status = 1
-        else:
-            if elementos[1] == 'CALL':
-                # chamada, salva o endereço da próxima instrução e pula para a função
-                endereco_proxima_instrucao = int(elementos[0]) + 1
-                return int(labels[elementos[2]])  # Retorna o endereço da função
-            elif elementos[1] == 'JUMP':
-                #salta para o endereço especificado
-                if elementos[2].startswith('R'):
-                    return int(registros[int(elementos[2][1:])])
-                else:
-                    return int(elementos[2])
-        mostraMemoria()
-    if elementos[1] == 'HALT':
-        print('FIM DO PROGRAMA.')
-        return -1
-    return int(elementos[0]) + 1
+  elementos = cmd.split(' ')
 
+  if len(elementos) == 3:
+
+    if elementos[1] == 'JUMP':
+      return int(elementos[2]) - 1
+
+    if elementos[1] == 'CALL':
+      return int(labels[elementos[2]]) - 1 
+
+    param = elementos[2].split(',')
+    p1 = param[0]
+    p2 = param[1]
+    posicaoDestino = int(p1[1:])
+
+    if elementos[1] == 'CMP':  
+      valorOrigem = registros[int(p2[1:])] if p2.startswith('R') else int(p2)
+      if registros[posicaoDestino] == valorOrigem: 
+        return int(elementos[0])
+      else:
+        return int(elementos[0]) + 1
+
+    if elementos[1] == 'LOAD':
+      if p2.startswith('R'):
+        registros[posicaoDestino] = registros[int(p2[1:])]
+      else:
+        registros[posicaoDestino] = int(p2)
+
+    if elementos[1] == 'ADD':
+      if p2.startswith('R'):
+        registros[posicaoDestino] = registros[posicaoDestino] + registros[int(p2[1:])]
+      else:
+        registros[posicaoDestino] = registros[posicaoDestino] + int(p2)
+
+    if elementos[1] == 'SUB':
+      if p2.startswith('R'):
+        registros[posicaoDestino] = registros[posicaoDestino] - registros[int(p2[1:])]
+      else:
+        registros[posicaoDestino] = registros[posicaoDestino] - int(p2)
+
+    if elementos[1] == 'MULT':
+      if p2.startswith('R'):
+        registros[posicaoDestino] = registros[posicaoDestino] * registros[int(p2[1:])]
+      else:
+        registros[posicaoDestino] = registros[posicaoDestino] * int(p2)
+
+    if elementos[1] == 'DIV':
+      if p2.startswith('R'):
+        if registros[int(p2[1:])] != 0:
+          registros[posicaoDestino] = registros[posicaoDestino] // registros[int(p2[1:])]
+        else:
+          print('Divisao por zero. ERRO')
+          return -1
+      else:
+        if int(p2) != 0:
+          registros[posicaoDestino] = registros[posicaoDestino] // int(p2)
+        else:
+          print('Divisao por zero. ERRO')
+          return -1
+
+    mostraMemoria()
+
+  if elementos[1] == 'HALT':
+    print('==============================================================')
+    return -1
+
+  return int(elementos[0])
 
 def executa():
-    try:
-        endMain = labels['main']
-        nrLinha = 0
-        print(endMain)
-        for l in linhasArquivo:
-            if l.startswith(endMain):
-                nrLinha = executaComando(l)
-                break
-        while nrLinha != -1:
-            if nrLinha < 10:
-                nomeLinha = '0' + str(nrLinha)
-            else:
-                nomeLinha = str(nrLinha)
-            for l in linhasArquivo:
-                if l.startswith(nomeLinha):
-                    print(l, 'linha')
-                    nrLinha = executaComando(l)
-                    break
-    except:
-        print('input nao possui main\nFim do programa.', Exception.__name__)
+  try:
+    nrLinha = 0
+    nomeLinha = 0
+    endMain = labels['main']
+
+    for l in linhasArquivo:  # p cada linha do arquivo...
+      if l.startswith(endMain):  # se a linha começar com o endereço do main
+        nrLinha = executaComando(l)
+        break
+
+    while nrLinha != -1:
+      if nrLinha is not None:
+        nomeLinha = '0' + str(nrLinha + 1) if (nrLinha + 1) <= 9 else str(nrLinha + 1)
+
+      for l in linhasArquivo:
+        if l.startswith(nomeLinha):
+          nrLinha = executaComando(l)
+          break
+
+  except IndexError:  # tratando os 2 erros específicos possíveis
+
+    contlabels = 0
+    flagMain = False
+
+    for l in linhasArquivo:
+      elementos = l.split(' ')
+      if len(elementos) == 1 and elementos[0].endswith(':'):
+        elementos[0] = elementos[0].replace(':', '')
+        contlabels += 1
+        if elementos[0] == 'main':
+          flagMain = True
+
+    if flagMain is False:
+      print("Input nao contem main.", Exception.__name__)
+      return Exception.__name__
+
+  except KeyError:
+    contlabels = 0
+    flagMain = False
+
+    for l in linhasArquivo:
+      elementos = l.split(' ')
+      if len(elementos) == 1 and elementos[0].endswith(':'):
+        elementos[0] = elementos[0].replace(':', '')
+        contlabels += 1
+        if elementos[0] == 'main':
+          flagMain = True
+
+    if flagMain is False:
+      print("Input nao contem main.", Exception.__name__)
+      return Exception.__name__
 
 
 def leArquivo(nomeArquivo):
-    global labels
-    with open(nomeArquivo) as arquivo:
+  global labels
+  with open(nomeArquivo) as arquivo:
+    flagLabel = False  # flag de leitura de label por padrão como falso
+    nomeLabel = None
+    for linha in arquivo:
+      elementos = linha.rstrip().split(' ')
+
+      if flagLabel:
+        labels[nomeLabel] = elementos[0]
         flagLabel = False
-        nomeLabel = None
-        for linha in arquivo:
-            elementos = linha.split(' ')
-            if flagLabel == True:
-                labels[nomeLabel] = elementos[0]
-                flagLabel = False
-            if linha.rstrip().endswith(':'):
-                flagLabel = True
-                nomeLabel = linha[:len(linha) - 2]
-            linhasArquivo.append(linha.rstrip())
-        arquivo.close()
+      if linha.rstrip().endswith(
+          ':'):  # se o conteudo da linha terminar com ':' (label)
+        flagLabel = True
+        nomeLabel = linha[:len(linha) - 2]
+      linhasArquivo.append(linha.rstrip())
 
 
 def mostraLinhas():
-    global linhasArquivo
-    for i in range(len(linhasArquivo)):
-        print(linhasArquivo[i])
+  global linhasArquivo
+  for i in range(len(linhasArquivo)):
+    print(linhasArquivo[i])
 
 
 def main():
-    memoria()
-    mostraMemoria()
-    i = input('Digite o nome do arquivo:')
-    leArquivo(i)
-    mostraLinhas()
-    print(labels)
-    executa()
+  memoria()  #constroi a memoria
+  mostraMemoria()
+  i = input('Digite o nome do arquivo: ')
+  print('\n')
+  leArquivo(i)
+  mostraLinhas()
+  print(f'\n\nlabels dictionary: {labels}')
+  executa()
 
 
 if __name__ == '__main__':
-    main()
-
+  main()
